ac7f4ef6f892206cb83157f3a16d09ac818bdf3c
Isabella Melo <isabella.melo@hashdex.com>
2022-11-01 02:02:29 -0300

    Adding a subject about routes

--- 03 - Fundamentos do Express.js e NPM/Express.js e NPM/index.js
+++ 03 - Fundamentos do Express.js e NPM/Express.js e NPM/index.js
@@ -1,11 +1,45 @@
 const express = require("express"); // Importando o express
 const app = express(); // Iniciando o express
 
+app.get("/", function(requisicao, resposta) {
+    resposta.send("Bem vindo!!"); // Resposta só pode enviar uma vez
+    // resposta.send("NãO PODE DUAS RESPOSTAS")
+});
+
+app.get("/blog", function(req, res) {
+    res.send("Bem vindo ao meu blog!!");
+});
+
+app.get("/ola/:nome/:sobrenome?", function(req, res) {
+    // REQUISIÇÃO: dados enviados pelo usuário
+    // Resposta: resposta que vai ser enviada para o usuário
+    var nome = req.params.nome;
+    var sobrenome = req.params.sobrenome;
+
+    if(sobrenome) {
+        res.send("Oi " + nome + " " + sobrenome + "!");
+    } else {
+        res.send("Oi " + nome + " !");
+    }
+});
+
+app.get("/canal/youtube", function(req, res) {
+    var canal = req.query["canal"]
+
+    if(canal) {
+        res.send(canal)
+    } else {
+        res.send("Nenhum canal fornecido!")
+    }
+});
+
 app.listen(4000, function(erro){
     if(erro){
         console.log("Ocorreu um erro!");
     } else {
-        console.log("Servidor iniciado com sucesso!")
+        console.log("Servidor iniciado com sucesso!");
     }
 })
+
+// npm install nodemon -g
 
