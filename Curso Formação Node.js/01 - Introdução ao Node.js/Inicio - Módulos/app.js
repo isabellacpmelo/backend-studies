a6405557bb39b4156dbe924f4276a3ef416732dc
isabellacpmelo <isacpmelo@gmail.com>
2023-08-08 20:50:40 -0300

    update: Changing first class content

--- Curso Formação Node.js/01 - Introdução ao Node.js/Inicio - Módulos/app.js
+++ Curso Formação Node.js/01 - Introdução ao Node.js/Inicio - Módulos/app.js
@@ -1,28 +1,16 @@
+console.log("Hello World!");
+console.log("Meu nome é Isabella!");
+console.log("E eu estou aprendendo Node.js com o Guia do programador");
+
+var mostrarSite = true;
+const site = "www.guiadoprogramador.com";
+if(mostrarSite){ // Se mostrar site
+    console.log(site); // Printe o site
+}
+
 var calculadora = require("./calculadora");
-
-
 
 console.log(calculadora.mult(10,20));
 console.log(calculadora.soma(20,40));
 calculadora.nome = "Calculadora da Isabella!"
 console.log(calculadora.nome);
-
-
-
-
-
-
-
-
-
-
-/*var mostrarSite = true;
-const site = "www.guiadoprogramador.com";
-
-console.log("Hello World!");
-console.log("Meu nome é Victor!");
-console.log("E eu estou aprendendo Node.js com o Guia do programador");
-
-if(mostrarSite){ // Se mostrar site
-    console.log(site); // Printe o site
-}*/
