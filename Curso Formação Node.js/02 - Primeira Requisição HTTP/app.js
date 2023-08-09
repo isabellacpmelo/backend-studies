955fa7fca607fbbd9fd30136d0570ebf368b6f86
isabellacpmelo <isacpmelo@gmail.com>
2023-08-08 21:49:34 -0300

    update: remaking class content

--- Curso Formação Node.js/02 - Primeira Requisição HTTP/app.js
+++ Curso Formação Node.js/02 - Primeira Requisição HTTP/app.js
@@ -1,6 +1,9 @@
 var http = require("http");
+// console.log(http);
 
-http.createServer(function(requisicao, resposta) {
+// http://localhost:8181/
+// A porta pode ser qualquer uma
+http.createServer(function(requisicao, resposta){
     resposta.end("<h1>Bem vindo ao meu site!</h1><h4>Isabella Melo</h4>")
 }).listen(8181);
-console.log('Meu servidor está rodando!')
+console.log('Meu servidor está rodando!');
