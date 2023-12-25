432e9a0fd15ffa98904358c8bed09bad90acf638
isabellacpmelo <isacpmelo@gmail.com>
2023-12-25 20:45:59 -0300

    Section 3: The print function

---
+++ Curson de Python 3/section_03/aula_01.py
@@ -0,0 +1,9 @@
+"""
+Aspas tripas também sao chamadas de Docstrings e são usadas para documentar o código em Python usando mais de uma linha.
+Também podem ser usadas aspas simples
+Elas não são usadas para comentários.
+"""
+
+# O hash e um caractere especial que indica que uma string contém uma linha de comentário
+
+print(123) # Isso e uma linha de comentário
---
+++ Curson de Python 3/section_03/aula_02.py
@@ -0,0 +1,5 @@
+# \r\n -> CRLF (Carriage Return + Line Feed)
+# \n -> LF (Line Feed)
+
+print(12, 34, sep="-", end='\r\n####\r\n')
+print(56, 78, sep='-', end='\n')
