5c14dc480ccbf9fae47545d987df55dd528fae6b
isabellacpmelo <isacpmelo@gmail.com>
2023-12-25 21:01:30 -0300

    Section 3: Type str (string) - Introduction to data types (Class 03)

---
+++ Curson de Python 3/section_03/aula_03.py
@@ -0,0 +1,19 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string (TEXTO)
+Strings são textos que estão dentro de aspas (simples ou duplas)
+"""
+
+# Aspas simples
+print('Isabella Melo', 'Usando aspas simples', sep=' - ')
+
+# Aspas duplas
+print("Isabella Melo", "Usando aspas duplas", sep=" - ")
+
+# Escape
+print("Isso é um escape: Aspas duplas (\")")
+print("Isso é outro escape: Aspas simples: (\')")
+
+# r -> raw string (nao faz o escape)
+print(r"Mostrando o caractere especial de escape: (\')")
