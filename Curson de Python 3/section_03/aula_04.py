ca87025f7302299fd2e57b80df1d6521dc10cdd8
isabellacpmelo <isacpmelo@gmail.com>
2023-12-27 22:23:54 -0300

    Section 03:  Type int e float (números) - Introduction to type of data (class 04)

---
+++ Curson de Python 3/section_03/aula_04.py
@@ -0,0 +1,15 @@
+# Tipos int e float
+# int -> Números inteiros
+# O tipo int representa qualquer número
+# positivo ou negativo. int sem sinal é considerado positivo.
+print(11, type(11), sep=' -> ')
+print(-11, type(-11), sep=' -> ')
+print(0, type(0), sep=' -> ')
+
+# float -> Número com ponto flutuante
+# O tipo float representa qualquer um número positivo ou negativo com ponto flutuante.
+# float sem sinal é considerado positivo.
+print(1.1, type(1.1), sep=' -> ')
+
+# A função type mostra o tipo que o Python inferiu ao valor
+print(type('Isabella'))
