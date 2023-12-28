8bb21bfa6e66fd9bf95aaf6f4e36f12377ce1ecb
isabellacpmelo <isacpmelo@gmail.com>
2023-12-27 22:30:03 -0300

    Section 03: Type bool (boolean) - Introdution to data types

---
+++ Curson de Python 3/section_03/aula_05.py
@@ -0,0 +1,7 @@
+# Tipo de dado bool (boleano)
+# Ao questionar algo em um programa só existem duas respostas possíveis:
+# Sim (True) ou Nao (False).
+# Existem duas operações para "questionar" algo em Python:
+# Dentre eles o == (igual) e o != (diferente), que são operadores lógicos e questionam se algo e o outro algo sao iguais ou diferentes
+
+print(10 == 10, 10 == 5, 10 != 5, sep='\n')
