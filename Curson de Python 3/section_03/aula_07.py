d7bdf5d88a1a55d5016d4fe57877ac6ea53760d1
isabellacpmelo <isacpmelo@gmail.com>
2023-12-31 14:52:33 -0300

    Section 03: Introduction to variables in Python (Class 07)

---
+++ Curson de Python 3/section_03/aula_07.py
@@ -0,0 +1,13 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variaveis com letras minúsculas, pode usar números e underlines _
+# O sinal de igual (=) é o operador de atribuição. Ele é usado para atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+primeiro_nome = 'Isabella'
+print(primeiro_nome)
+
+soma_dois_mais_dois = 2 + 2
+print(soma_dois_mais_dois)
+
+int_um = int('1')
+print(int_um, type(int_um), sep=' -> ')
