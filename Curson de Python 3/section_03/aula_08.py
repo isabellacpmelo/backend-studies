e4e4291a37ab95c7b63b92a69577bab17380473a
isabellacpmelo <isacpmelo@gmail.com>
2023-12-31 14:58:25 -0300

    Section 03: Exercise with Variables and Data Types (Class 08)

---
+++ Curson de Python 3/section_03/aula_08.py
@@ -0,0 +1,13 @@
+nome = 'Icaro'
+sobrenome = 'Randsome'
+idade = 68
+ano_nascimento = 1050
+maior_de_idade = True
+altura_metros = 3.7
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_nascimento)
+print('E maior de idade: ', maior_de_idade)
+print('Altura em metros: ', altura_metros)
