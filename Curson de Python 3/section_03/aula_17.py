8e0669e260f5a486e17c9f97a9eac6b58f4f531a
isabellacpmelo <isacpmelo@gmail.com>
2024-01-01 13:41:32 -0300

    Section 03: if, elif and else - Understanding the interpreter flow in conditionals (Class 17)

---
+++ Curson de Python 3/section_03/aula_17.py
@@ -0,0 +1,13 @@
+condicao = False
+
+if condicao:
+    print('Este é o if código do primeiro if')
+else:
+    print('Este é o else do primeiro if')
+
+if 10 == 10:
+    print('Este é o código do segundo if')
+else:
+    print('Este é o else do segundo if')
+
+print('Fora dos ifs')
