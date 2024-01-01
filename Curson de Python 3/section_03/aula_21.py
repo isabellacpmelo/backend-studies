6496d4b4267ddc54a6604a6c741462311c826c13
isabellacpmelo <isacpmelo@gmail.com>
2024-01-01 17:49:05 -0300

    Section 03: Logical operator "not" (Class 21)

---
+++ Curson de Python 3/section_03/aula_21.py
@@ -0,0 +1,9 @@
+# Operador lógico not
+# Usado para inverter o valor de expressões
+# not True = False
+# not False = True
+
+senha = input('Senha: ')
+
+if not senha:
+    print('Voce deve informar uma senha!')
