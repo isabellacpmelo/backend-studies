5f2310bb234dd8d22693a31ac12587f8755374cf
isabellacpmelo <isacpmelo@gmail.com>
2024-01-01 13:27:29 -0300

    Section 03: Using the input function to collect user data (Class 15)

---
+++ Curson de Python 3/section_03/aula_15.py
@@ -0,0 +1,11 @@
+nome = input('Qual o seu nome? ')
+print(f'Prazer em te conhecer {nome}!')
+print(f'{nome=}')
+
+numero_1 = input('Digite um numero: ')
+numero_2 = input('Digite outro numero: ')
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'A soma de {int_numero_1} mais {int_numero_2} é {int_numero_1 + int_numero_2}')
