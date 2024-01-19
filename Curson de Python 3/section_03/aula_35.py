32f4373eebbc22c10927f0a66b7c9e6b1a9f1e08
isabellacpmelo <isacpmelo@gmail.com>
2024-01-19 16:56:28 -0300

    Section 03: Assignment Operators with Arithmetic Operators (Class 35)

---
+++ Curson de Python 3/section_03/aula_35.py
@@ -0,0 +1,19 @@
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+= Operador de atribuição
++= Operador de adição e atribuição
+-= Operador de subtração e atribuição
+*= Operador de multiplicação e atribuição
+/= Operador de divisão e atribuição
+//= Operador de divisão inteira e atribuição
+**= Operador de potência e atribuição
+%= Operador de resto e atribuição
+"""
+
+contador = 1
+
+###
+
+contador += 2
+print(contador)
